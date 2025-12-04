82211890e052814797ed75ab90668cfe751057f3
ndelh <nathan.delhotal@gmail.com>
2025-12-04 12:57:27 +0100

    test

--- trans.sh
+++ trans.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -e
-echo "must be runned with sudo activated" > logs
+echo "must be runned with root activated"
 echo "installing vim" >> logs
-sudo apt update
-sudo apt install vim
-sudo apt install tree
+apt update
+apt upgrade
+apt install vim
+apt install tree
 echo "removing eventual docker system" >> logs
-sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
+apt remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
 # Add Docker's official GPG key:
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/debian
 Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
@@ -22,8 +23,9 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-sudo apt update
+apt update
 echo "installing docker engine, version released the 28/11" >> logs
-VERSION_STRING=5:29.1.1-1~debian.12~bookworm
-sudo apt install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
-
+VERSION_STRING=5:29.1.1-1~debian.13~trixie
+apt install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker $USERNAME
+systemctl status docker
